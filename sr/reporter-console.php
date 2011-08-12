18f9434aa3c8928ca25c96eb0959634f0cf152d9
niravmehta <niravmehta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-12 12:37:06 +0000

    Changes for 1.3
    git-svn-id: https://plugins.svn.wordpress.org/smart-reporter-for-wp-e-commerce/trunk@422591 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sr/reporter-console.php
+++ sr/reporter-console.php
@@ -1,7 +1,20 @@
-<div id="smart-reporter"></div>
 <?php 
+// to set javascript variable of file exists
+$fileExists = (SRPRO === true) ? 1 : 0;
+$selectDateValue = (SRPRO === true) ? 'THIS_MONTH' : 'LAST_SEVEN_DAYS';
+
+if ($fileExists){
+	if(file_exists ( SR_PLUGIN_DIR. '/pro/sr.php' )){
+		include_once( SR_PLUGIN_DIR. '/pro/sr.php' );
+	}
+}
+
 echo "<script type='text/javascript'>
-var jsonURL = '".SR_JSON_URL."';
-var imgURL  = '".SR_IMG_URL . "';
+var jsonURL    = '" .SR_JSON_URL. "';
+var imgURL     = '" .SR_IMG_URL . "';
+var fileExists = '" .$fileExists. "';
+var selectDateValue = '" .$selectDateValue. "';
 </script>";
 ?>
+<br>
+<div id="smart-reporter"></div>
